d2173f5cd3760c45167ada9cfdcfd31dd7e34ea9
Hackbright Student <no-reply@hackbrightacademy.com>
2018-03-21 18:02:17 -0700

    completed add_to_cart view

--- shoppingsite.py
+++ shoppingsite.py
@@ -6,7 +6,7 @@ put melons in a shopping cart.
 Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
@@ -48,7 +48,7 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id("meli")
+    melon = melons.get_by_id(melon_id)
     print melon
     return render_template("melon_details.html",
                            display_melon=melon)
@@ -87,18 +87,19 @@ def add_to_cart(melon_id):
     page and display a confirmation message: 'Melon successfully added to
     cart'."""
 
-    # TODO: Finish shopping cart functionality
 
-    # The logic here should be something like:
-    #
-    # - check if a "cart" exists in the session, and create one (an empty
-    #   dictionary keyed to the string "cart") if not
-    # - check if the desired melon id is the cart, and if not, put it in
-    # - increment the count for that melon id by 1
-    # - flash a success message
-    # - redirect the user to the cart page
+    if "cart" in session:
+        #update the value of cart
+        if melon_id in session['cart']:
+            session['cart'][melon_id] += 1
 
-    return "Oops! This needs to be implemented!"
+        else:
+            session['cart'][melon_id] = 1
+    else:
+        session['cart'] = {melon_id: 1}
+
+    flash("Added {} to cart".format(melon_id))
+    return redirect("/cart")
 
 
 @app.route("/login", methods=["GET"])
